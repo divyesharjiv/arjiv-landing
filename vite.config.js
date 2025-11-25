0e719d2de7ade7f2f83adef80340f324114240c5
Divyesh Software <divyesh.arjiv@gmail.com>
2025-11-25 14:31:07 +0530

    Vercel Settings

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.glb", "**/*.gltf"],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve("./src"),
